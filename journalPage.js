90ef131c4d608a8e73999ce935607f01b3faf879
Jessieljx118 <90782027+Jessieljx118@users.noreply.github.com>
2022-11-23 00:54:12 -0500

    Roughly complete the page

---
+++ journalPage.js
@@ -0,0 +1,16 @@
+function function0(){
+    var arr = $("input[name='myTextInput']").map(function() {
+        return this.value;
+    }).get();//use jquery map() to put text in array ?
+}
+//another method set a array to reserve maybe?
+//var myFormData = [];   //declare an array
+//var value1 = document.getElementById("myTextInput").value;  //get the value of an element by it's id
+//myFormData.push(value1); //put to the array
+
+function function1(){
+    var ratingstar = prompt("rating stars please enter start number you want to give：");
+    //can not use popover to implement the option to select stars ? how to modify it?
+    
+}
+
