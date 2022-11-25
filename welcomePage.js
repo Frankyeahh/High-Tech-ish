4a2dadd78ac671e4d38c6e522d455baa15817c77
Liguangxuan He <catherine.he728@gmail.com>
2022-11-25 18:39:49 -0500

    welcomePage.css

--- welcomePage.js
+++ welcomePage.js
@@ -2,18 +2,20 @@ function journalPageSwitch(){
     
 }
 
-function dropdown(){
+function dropdown() {
     document.getElementById("myDropdown").classList.toggle("show");
-}
-window.onclick = function(event){
-    if (!event.target.matches('.dropbtn')){
+    }
+    
+    // Close the dropdown menu if the user clicks outside of it
+    window.onclick = function(event) {
+      if (!event.target.matches('.dropbtn')) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
         var i;
-        for (i = 0; i < dropdowns.length; i++){
-            var openDropdown = dropdowns[i];
-            if(openDropdown.classList.contains('show')){
-                openDropdown.classList.remove('show');
-            }
+        for (i = 0; i < dropdowns.length; i++) {
+          var openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains('show')) {
+            openDropdown.classList.remove('show');
+          }
         }
-    }
-}
+      }
+    } 
